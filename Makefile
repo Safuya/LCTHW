25d6cac8e66d114143ad5f74f29fd50aa0c26169
Robert Hughes <rhughes@localhost.localdomain>
2015-03-06 21:08:19 +0000

    Initial commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+CFLAGS=-Wall -g
+
+clean:
+	rm -f ex1_hello_world
+	rm -f ex3_printf
---
+++ ex3_printf.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+int main() {
+    int age = 10;
+    int height = 72;
+
+    printf("I am %d years old.\n", age);
+    printf("I am %d inches tall.\n", height);
+    printf("Hello\x21\n");
+
+    return 0;
+}
