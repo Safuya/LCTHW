7a7a5e60b3087c90d030186c2a4e1288cbfcb529
Robert Hughes <rhughes@localhost.localdomain>
2015-03-07 21:59:47 +0000

    Ex5 and 6 complete

--- Makefile
+++ Makefile
@@ -4,3 +4,5 @@ clean:
 	rm -f ex1_hello_world
 	rm -f ex3_printf
 	rm -f ex4_broken
+	rm -f ex5_structure
+	rm -f ex6_var_types
---
+++ ex5_structure.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* Multiline comment. int argc tells how many arguments to take. char *argv[] tells it to take inifinite chars */
+int main(int argc, char *argv[]) {
+    int distance = 100;
+
+	// Single line comment
+	printf("You are %d miles away.\n", distance);
+
+	return 0;
+}
